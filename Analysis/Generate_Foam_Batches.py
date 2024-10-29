522f070dfc001877d60ff0d94ca75bb3dc32951a
Optiplex_7060 <jackericson98@gmail.com>
2024-10-29 10:57:46 -0400

    Minor changes

--- Analysis/Generate_Foam_Batches.py
+++ Analysis/Generate_Foam_Batches.py
@@ -4,7 +4,6 @@ import numpy as np
 import os
 
 
-
 if __name__ == '__main__':
     # Open the folder
     root = tk.Tk()
@@ -13,11 +12,13 @@ if __name__ == '__main__':
     folder = filedialog.askdirectory()
 
     # Set the settings
-    cv_vals = {0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0}
-    density_vals = {0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5}
-    my_string = "python foam_gen.py cv {:.3f} den {:.3f} num 1000 olp 0.0 dist gamma pbc t\n"
-    number_of_foam_files = 20
-    number_of_files = 5
+    # cv_vals = {0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0}
+    # density_vals = {0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1.0}
+    cv_vals = {0.25, 0.5}
+    density_vals = {0.5}
+    my_string = "python foam_gen.py cv {:.3f} den {:.3f} num 1000 olp 0.5 dist gamma pbc t\n"
+    number_of_foam_files = 50
+    number_of_files = 1
 
     # Create the dictionary
     dir_dict = {}
@@ -36,16 +37,17 @@ if __name__ == '__main__':
     # Create the new strings list
     new_strings = []
     # Now loop through the cv_vals and density vals
-    for cv in cv_vals:
-        for density in density_vals:
-            if (cv, density) in dir_dict:
-                for _ in range(number_of_foam_files - len(dir_dict[(cv, density)])):
-                    new_strings.append(my_string.format(cv, density))
-            else:
-                for _ in range(20):
+    for _ in range(number_of_foam_files):
+        for cv in cv_vals:
+            for density in density_vals:
+                if (cv, density) in dir_dict:
+                    for _ in range(number_of_foam_files - len(dir_dict[(cv, density)])):
+                        new_strings.append(my_string.format(cv, density))
+                else:
                     new_strings.append(my_string.format(cv, density))
 
     for i, stank in enumerate(new_strings):
-        file_number = i // (len(new_strings) / number_of_files)
-        with open('../foam_gen_{}.sh'.format(file_number), 'a') as foam_writer:
+        file_number = int(i // (len(new_strings) / number_of_files))
+        j = file_number + 1
+        with open('../foam_gen_{}.sh'.format(j), 'a') as foam_writer:
             foam_writer.write(stank)
