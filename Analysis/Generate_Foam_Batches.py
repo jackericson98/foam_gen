b7961cae8a71509a8fd5c502ce0d9a1fcd71c516
e5-2696v2 <jackericson98@gmail.com>
2024-12-18 18:32:23 -0500

    The foam generation is pure and beautiful and I couldn't be more proud of myself (chatGPT)

--- Analysis/Generate_Foam_Batches.py
+++ Analysis/Generate_Foam_Batches.py
@@ -28,15 +28,16 @@ if __name__ == '__main__':
     # Set the settings
     means = [1.0]
     cv_vals = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
-    density_vals = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
-    olap_vals = [0.05]
+    density_vals = [0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5]
+    olap_vals = [0.0]
     num_vals = [1000]
     pbc_vals = ['False']
     dist_vals = ['gamma']
     my_string = "python foam_gen.py mean {:.3f} cv {:.3f} num {} den {:.3f} olp {} dist {} pbc {} \n"
     number_of_foam_files = 20
-    number_of_files = 4
-
+    number_of_files = 10
+    # Count the number of foams that need to be created
+    total_number_of_foams = 0
     # Create the new strings list
     new_strings = []
     # Now loop through the cv_vals and density vals
@@ -48,15 +49,31 @@ if __name__ == '__main__':
                         for pbc in pbc_vals:
                             for density in density_vals:
                                 if (mean, cv, num, density, olap, dist, pbc) in count_dict:
-                                    num_files_to_make = number_of_foam_files - count_dict[(mean, cv, num, density, olap, dist, pbc)]
+                                    count_dict[(mean, cv, num, density, olap, dist, pbc)] = number_of_foam_files - count_dict[(mean, cv, num, density, olap, dist, pbc)]
                                 else:
-                                    num_files_to_make = number_of_foam_files
-                                for _ in range(num_files_to_make):
+                                    count_dict[(mean, cv, num, density, olap, dist, pbc)] = number_of_foam_files
+                                # Add to the total number of foams
+                                total_number_of_foams += count_dict[(mean, cv, num, density, olap, dist, pbc)]
 
-                                    new_strings.append(my_string.format(mean, cv, num, density, olap, dist, pbc))
 
-    for i, stank in enumerate(new_strings):
-        file_number = int(i // (len(new_strings) / number_of_files))
-        j = file_number + 1
-        with open('../foam_gen_{}.sh'.format(j), 'a') as foam_writer:
-            foam_writer.write(stank)
+    # Sort the dictionary
+    count_dict = {key: value for key, value in
+                  # Sort by number, reverse overlap, density and then reverse cv
+                  sorted(count_dict.items(), key=lambda item: (item[0][2], -item[0][4], item[0][3], -item[0][1]))}
+
+    # Set the counter for the number of lines
+    counter = 0
+    # Loop through the remaining runs
+    for vals in count_dict:
+        # Loop through the number of times each run needs to be made
+        for i in range(count_dict[vals]):
+            # Get the file number based on the counter so we know when a new file needs to start
+            file_number = int(counter // (total_number_of_foams / number_of_files))
+            j = file_number + 1
+
+            # Open the file we intend to write to
+            with open(f'../foam_gen_{j}.sh', 'a') as foam_writer:
+                foam_writer.write(my_string.format(*vals))
+
+            # Update the counter
+            counter += 1
